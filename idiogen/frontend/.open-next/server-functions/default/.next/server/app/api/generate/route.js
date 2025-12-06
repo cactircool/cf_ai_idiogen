9ca980459253a8966a5ff83bffe43353cc8b43ac
cactircool <c4ctircool@gmail.com>
2025-12-06 00:13:02 +0000

    more

--- idiogen/frontend/.open-next/server-functions/default/.next/server/app/api/generate/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/generate/route.js")
-R.c("server/chunks/[root-of-the-server]__6e809cc1._.js")
-R.c("server/chunks/[root-of-the-server]__8f5ebbc3._.js")
-R.c("server/chunks/_next-internal_server_app_api_generate_route_actions_5bfe9259.js")
-R.m(95434)
-module.exports=R.m(95434).exports
--- idiogen/frontend/.open-next/server-functions/default/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"htO14i1EJ1u7ZgUgODInlzrAjh3xRdrS2MqxVvAREQM=\"\n}"
