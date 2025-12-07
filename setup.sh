a996fe7ee08c151ebb8c02caa32c64eadaa7347c
cactircool <c4ctircool@gmail.com>
2025-12-07 09:32:31 +0000

    deploy workflow in setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(dirname $0)
+
+cd $SCRIPT_DIR/workflow
+npm run types
+npm run deploy
+
 sudo $SCRIPT_DIR/builder/setup.sh $SCRIPT_DIR/builder/main.go
 sudo $SCRIPT_DIR/frontend/setup.sh $SCRIPT_DIR/frontend
