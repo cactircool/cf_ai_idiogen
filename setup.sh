83ba6ab6aaad4d8ee5a2d5c075e233c57cf4fb01
cactircool <c4ctircool@gmail.com>
2025-12-07 10:30:18 +0000

    updated setup script

--- setup.sh
+++ setup.sh
@@ -2,10 +2,14 @@
 
 SCRIPT_DIR=$(dirname $0)
 
-cd $SCRIPT_DIR/workflow
-npm install
-npm run types
-npm run deploy
+read -p "Deploy workflow on setup? (requires a GUI browser on current device) " d_workflow
+
+if [ "$d_workflow" = "Y" ] || [ "$d_workflow" = "y" ]; then
+	cd $SCRIPT_DIR/workflow
+	npm install
+	npm run types
+	npm run deploy
+fi
 
 sudo $SCRIPT_DIR/builder/setup.sh $SCRIPT_DIR/builder/main.go
 sudo $SCRIPT_DIR/frontend/setup.sh $SCRIPT_DIR/frontend
